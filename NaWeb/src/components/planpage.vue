eccf7959872ec25ce299423293d0958ec3bf9ffb
MistySky <2251154218@qq.com>
2017-09-30 16:25:42 +0800

    修改官网样式

--- NaWeb/src/components/planpage.vue
+++ NaWeb/src/components/planpage.vue
@@ -22,8 +22,17 @@
             </div>
             <p>{{item.content}}</p>
             <h3>【大奖】</h3>
+            <div class="msgImg">
+              <img :src="item.img1" alt="">
+            </div>
             <h3>【特色选择模式】</h3>
+            <div class="msgImg">
+              <img :src="item.img2" alt="">
+            </div>
             <h3>【免费游戏】</h3>
+            <div class="msgImg">
+              <img :src="item.img3" alt="">
+            </div>
           </div>
         </div>
       </div>
@@ -45,12 +54,18 @@ export default {
         {
           title: '塔罗之谜',
           imgUrl: '/static/ele.png',
-          content: '  塔罗题材的电子游戏，有着小阿卡那牌【宝剑】【权杖】【圣杯】【星币】元素的符号预示着命运的真相。当出现3个及以上的【命运之轮】时，代表着你即将做一个重大的选择，五色卡牌分别对应五种决定命运的水晶球，跟随着直觉做出选择，带你进入全新的塔罗世界，感受塔罗的神秘力量。'
+          content: '  塔罗题材的电子游戏，有着小阿卡那牌【宝剑】【权杖】【圣杯】【星币】元素的符号预示着命运的真相。当出现3个及以上的【命运之轮】时，代表着你即将做一个重大的选择，五色卡牌分别对应五种决定命运的水晶球，跟随着直觉做出选择，带你进入全新的塔罗世界，感受塔罗的神秘力量。',
+          img1: '/static/tl_big.jpg',
+          img2: '/static/tl_choise.jpg',
+          img3: '/static/tl_free.jpg'
         },
         {
           title: '小厨娘',
           imgUrl: '/static/ele2.jpg',
-          content: '有着传统中国风的美食题材电子游戏。一个民间普通的小厨娘，通过烹饪中国特色美食【北京烤鸭】【东坡肘子】【小笼包】【麻婆豆腐】【水煮鱼】受到皇帝了的青睐，下发御用【令牌】传召小厨娘进宫参加厨艺比赛。随着令牌传到的还有5卷诏书，小厨娘在选择一款拿手的调味方式后，将身着霓裳，进入华丽的皇宫参加比赛，根据自身的表现获得奖励。'
+          content: '有着传统中国风的美食题材电子游戏。一个民间普通的小厨娘，通过烹饪中国特色美食【北京烤鸭】【东坡肘子】【小笼包】【麻婆豆腐】【水煮鱼】受到皇帝了的青睐，下发御用【令牌】传召小厨娘进宫参加厨艺比赛。随着令牌传到的还有5卷诏书，小厨娘在选择一款拿手的调味方式后，将身着霓裳，进入华丽的皇宫参加比赛，根据自身的表现获得奖励。',
+          img1: '/static/xcn_big.jpg',
+          img2: '/static/xcn_choise.jpg',
+          img3: '/static/xcn_free.jpg'
         }
       ],
       productData: [],
@@ -58,7 +73,10 @@ export default {
         {
           title: this.GetQueryString('title'),
           imgUrl: '/static/msg.png',
-          content: '这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容'
+          content: '这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容这是测试内容',
+          img1: '/static/msg.png',
+          img2: '/static/msg.png',
+          img3: '/static/msg.png'
         }
       ],
       title: ''
@@ -149,13 +167,13 @@ export default {
       .msgContent {
         padding: 50px;
         h1 {
-          margin: 50px auto;
+          margin: 100px auto 50px;
           font-size: 40px;
           color: #c00000;
         }
         .msgImg {
           width: 635px;
-          height: 250px;
+          // height: 250px;
           margin: 0 auto;
           img {
             width: 100%;
@@ -165,13 +183,13 @@ export default {
         p {
           margin: 0 auto 20px;
           margin-top: 18px;
-          width: 925px;
+          width: 635px;
           color: #7e7e7e;
           font-size: 16px;
           line-height: 30px
         }
         h3 {
-          margin: 20px 0;
+          margin: 100px 0 50px;
         }
       }
     }
@@ -213,13 +231,13 @@ export default {
           }
           .msgImg {
             width: 425px;
-            height: 165px;
+            // height: 165px;
             margin: 0 auto;
           }
           p {
             margin: 0 auto 40px;
             margin-top: 10px;
-            width: 620px;
+            width: 425px;
             font-size: 10.67px;
             line-height: 20px
           }
