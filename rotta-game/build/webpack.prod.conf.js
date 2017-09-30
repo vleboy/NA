062d4257dca1b25bc11adeb9a4dc3bbdb4c206cc
fu wei <592197848@qq.com>
2017-09-30 11:38:48 +0800

    配置webpack不打包源文件

--- rotta-game/build/webpack.prod.conf.js
+++ rotta-game/build/webpack.prod.conf.js
@@ -46,7 +46,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: false
     }),
     // extract css into its own file
     new ExtractTextPlugin({
--- rotta-merchant/build/webpack.prod.conf.js
+++ rotta-merchant/build/webpack.prod.conf.js
@@ -48,7 +48,7 @@ var webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: false
     }),
     // extract css into its own file
     new ExtractTextPlugin({
