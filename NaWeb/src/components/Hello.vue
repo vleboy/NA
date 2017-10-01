0da9dc2ea5af953af33d87f8913ff54bad7a49bd
MistySky <2251154218@qq.com>
2017-10-01 11:40:42 +0800

    修改资源位https

--- NaWeb/src/components/Hello.vue
+++ NaWeb/src/components/Hello.vue
@@ -60,5 +60,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang='less'>
-@import "http://cdn.dowebok.com/77/css/jquery.fullPage.css";
 </style>
