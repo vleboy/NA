e9c7e44e1407cc1ae5642dba3e61a6923b29fe2e
MistySky <2251154218@qq.com>
2017-10-13 23:20:28 +0800

    修复样式显示bug

--- NaWeb/src/components/Hello.vue
+++ NaWeb/src/components/Hello.vue
@@ -1,29 +1,34 @@
 <template>
   <div>
-    <!-- <div id="dowebok" v-if="isShow">
+    <div id="dowebok">
       <div class="section">
-        <Slider></Slider>
+        <Slider v-show="show" ></Slider>
+        <Slider2 v-show="!show"></Slider2>
       </div>
       <div class="section">
-        <About></About>
+        <About v-show="show"></About>
+        <About2 v-show="!show"></About2>
       </div>
       <div class="section">
-        <Download></Download>
+        <Download v-show="show"></Download>
+        <Download2 v-show="!show"></Download2>
       </div>
       <div class="section">
         <Plan></Plan>
       </div>
       <div class="section">
-        <Product></Product>
+        <Product v-show="show"></Product>
+        <Product2 v-show="!show"></Product2>
       </div>
       <div class="section">
-        <N2></N2>
+        <N2 v-show="show"></N2>
+        <N22 v-show="!show"></N22>
       </div>
       <div class="section">
         <Contact></Contact>
       </div>
     </div>
-    <div id="doweboks" v-if="!isShow">
+    <!-- <div id="doweboks" v-if="!isShow">
       <div class="section">
         <Slider2></Slider2>
       </div>
@@ -45,67 +50,47 @@
       <div class="section">
         <Contact></Contact>
       </div>
-    </div> -->
-  <pc v-if="isShow" />
-  <mobile v-else />
+    </div>  -->
+  <!-- <pc v-if="isShow" />
+  <mobile v-else /> -->
   </div>
 </template>
 
 <script>
-// import Slider from './slider'
-// import Slider2 from './slider2'
-// import About from './about'
-// import About2 from './About2'
-// import Download from './download'
-// import Download2 from './download2'
-// import Plan from './plan'
-// import Product from './product'
-// import Product2 from './product2'
-// import N2 from './n2'
-// import N22 from './n22'
-// import Contact from './contact'
-import pc from './pc'
-import mobile from './mobile'
+import Slider from './slider'
+import Slider2 from './slider2'
+import About from './about'
+import About2 from './About2'
+import Download from './download'
+import Download2 from './download2'
+import Plan from './plan'
+import Product from './product'
+import Product2 from './product2'
+import N2 from './n2'
+import N22 from './n22'
+import Contact from './contact'
 export default {
   name: 'hello',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App',
-      isShow: true
+      show: true
     }
   },
   mounted() {
-    // if(new RegExp(/(nokia|iphone|android|ipad|motorola|^mot\-|softbank|foma|docomo|kddi|up\.browser|up\.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam\-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte\-|longcos|pantech|gionee|^sie\-|portalmmm|jig\s browser|hiptop|^ucweb|^benq|haier|^lct|opera\s*mobi|opera\*mini|320x320|240x320|176x220)/i).test(navigator.userAgent)){
-    //   console.log("m")
-    //   this.isShow = false
-    //   $('#myContainer').fullpage (
-    //   {
-    //     scrollingSpeed: 500,
-    //     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
-    //     menu: '#menu'
-    //   }
-    // )
-    // } else {
-    //   console.log("p")
-    //   this.isShow = true
-    //   $('#dowebok').fullpage (
-    //   {
-    //     scrollingSpeed: 500,
-    //     anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
-    //     menu: '#menu'
-    //   }
-    // )
-    // }
-    var $ww = $(window).width();
-    if($ww < 768){
-      this.isShow = false
+    $('#dowebok').fullpage ({
+      scrollingSpeed: 500,
+      anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6', 'page7'],
+      menu: '#menu'
+    })
+    if(new RegExp(/(nokia|iphone|android|ipad|motorola|^mot\-|softbank|foma|docomo|kddi|up\.browser|up\.link|htc|dopod|blazer|netfront|helio|hosin|huawei|novarra|CoolPad|webos|techfaith|palmsource|blackberry|alcatel|amoi|ktouch|nexian|samsung|^sam\-|s[cg]h|^lge|ericsson|philips|sagem|wellcom|bunjalloo|maui|symbian|smartphone|midp|wap|phone|windows ce|iemobile|^spice|^bird|^zte\-|longcos|pantech|gionee|^sie\-|portalmmm|jig\s browser|hiptop|^ucweb|^benq|haier|^lct|opera\s*mobi|opera\*mini|320x320|240x320|176x220)/i).test(navigator.userAgent)){
+      this.show = false
     } else {
-    this.isShow = true
+    this.show = true
     }
   },
   components: {
-    mobile, pc
-    // Slider, About, Plan, Product, N2, Contact, Download, Slider2, About2, Download2, Product2, N22
+    Slider, About, Plan, Product, N2, Contact, Download, Slider2, About2, Download2, Product2, N22
   }
 }
 </script>
--- NaWeb/src/components/n22.vue
+++ NaWeb/src/components/n22.vue
@@ -46,7 +46,7 @@ export default {
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .n2 {
   width: 100%;
   height: 100%;
