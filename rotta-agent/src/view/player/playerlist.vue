1c23b3ec02f8b2e89cbff3d89e2b20b4e6ef4571
fu wei <592197848@qq.com>
2017-10-24 15:34:39 +0800

    代理玩家列表分页数量修改

--- rotta-agent/src/view/player/playerlist.vue
+++ rotta-agent/src/view/player/playerlist.vue
@@ -82,7 +82,7 @@
       </el-table>
       <div style="text-align: right;margin:2rem 0">
         <el-pagination layout="prev, pager, next, sizes, jumper" :total="playerList.length"
-                       :page-sizes="[5, 10]" :page-size="nowSize" @size-change="getNowsize" @current-change="getNowpage">
+                       :page-sizes="[20, 50]" :page-size="nowSize" @size-change="getNowsize" @current-change="getNowpage">
         </el-pagination>
       </div>
     </div>
@@ -124,7 +124,7 @@
     },
     data () {
       return {
-        nowSize: 5,
+        nowSize: 20,
         nowPage: 1,
         showSearch: false,
         isOpenModal: false,
