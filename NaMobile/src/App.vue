91bc8337543ae92a76623e480a018ee1364d78db
MistySky <2251154218@qq.com>
2017-11-06 18:38:15 +0800

    更新安装包

--- NaMobile/src/App.vue
+++ NaMobile/src/App.vue
@@ -72,7 +72,7 @@ export default {
         // .catch(function (error) {
         //   console.log(error)
         // })
-        window.location.href = 'itms-services://?action=download-manifest&url=https://oss.na12345.com/autoinstall1320.plist'
+        window.location.href = 'itms-services://?action=download-manifest&url=https://oss.na12345.com/autoinstall1400.plist'
       }
     }else if (is_weixin() || is_QQInnerBro()) {
       this.show = true
--- NaWeb/src/components/download2.vue
+++ NaWeb/src/components/download2.vue
@@ -7,7 +7,7 @@
         </h2>
         <div class="download-address">
             <div class="android"><a href="http://www.na77.com/NAGame.apk"><img src="../assets/e1.png" alt=""></a></div>
-            <div class="ios"><a href="itms-services://?action=download-manifest&url=https://oss.na12345.com/autoinstall1320.plist"><img src="../assets/e2.png" alt=""></a></div>
+            <div class="ios"><a href="itms-services://?action=download-manifest&url=https://oss.na12345.com/autoinstall1400.plist"><img src="../assets/e2.png" alt=""></a></div>
         </div>
       </div>
   </div>
