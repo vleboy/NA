9f7abaeea6765822f73a7c05b394cba28fa02391
fu wei <592197848@qq.com>
2017-11-08 15:26:41 +0800

    修改详情页编辑bug

--- rotta-admin/src/view/agent/out-agent/outdetail.vue
+++ rotta-admin/src/view/agent/out-agent/outdetail.vue
@@ -822,6 +822,7 @@ export default {
         type: 'recordComdetailID',
         data: ID
       })
+      this.$store.commit('closeEdit')
       this.$router.push('comdetail')
       this.$store.commit('startLoading')
       this.$store.dispatch('getComdetail')
@@ -889,6 +890,7 @@ export default {
         type: 'recordOutdetailID',
         data: this.outdetail.parent
       })
+      this.$store.commit('closeEdit')
       this.$router.push('outdetail')
       this.$store.commit('startLoading')
       this.$store.dispatch('getOutdetail')
