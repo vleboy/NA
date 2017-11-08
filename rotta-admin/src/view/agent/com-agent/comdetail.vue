57799a2abd301f46c51180062d00b97dcc6ba6d0
fu wei <592197848@qq.com>
2017-11-08 16:01:12 +0800

    修改游戏重复循环问题

--- rotta-admin/src/view/agent/com-agent/comdetail.vue
+++ rotta-admin/src/view/agent/com-agent/comdetail.vue
@@ -702,6 +702,7 @@ export default {
           const [err, ret] = result
           if (err) {
           } else {
+            this.parentGamelist = []
             var data = ret.data.payload
             this.gameList = data
             for(let item of data) {
