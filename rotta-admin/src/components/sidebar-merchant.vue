2dc9f5622d22adea224b4829a276cff4ad4a91d6
gp <gp646890523@sina.com>
2017-12-11 11:18:44 +0800

    update ( rotta-admin )  站内信更改为邮件

--- rotta-admin/src/components/sidebar-merchant.vue
+++ rotta-admin/src/components/sidebar-merchant.vue
@@ -23,7 +23,7 @@
                   <el-menu-item index="horseRaceLampList">跑马灯列表</el-menu-item>
                 </el-submenu>
                 <el-submenu index="4-2">
-                  <template slot="title">站内信管理</template>
+                  <template slot="title">邮件管理</template>
                   <el-menu-item index="gameMailList">游戏邮件列表</el-menu-item>
                 </el-submenu>
                 <el-submenu index="4-3">
