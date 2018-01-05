c01691f4d14ef167e7fb1037168e431dca9b4973
gp <gp646890523@sina.com>
2018-01-05 10:45:55 +0800

    update ( rotta )  代理平台，管理员平台玩家详细时间搜索bug修复

--- rotta-admin/src/view/player/playerdetail.vue
+++ rotta-admin/src/view/player/playerdetail.vue
@@ -353,8 +353,8 @@ export default {
       )
     },
     initTime () {
-      const start = new Date();
-      const end = new Date();
+      const start = this.amountDate[0] ? new Date(this.amountDate[0]) : new Date();
+      const end = this.amountDate[1] ? new Date(this.amountDate[1]) : new Date();
       start.setTime(start.getTime() - 3600 * 1000 * 24 * 6);
       this.amountDate = [start,end];
     }
