c0d77c7690dcc0af3f40f2330570465dbd06e5a6
fu wei <592197848@qq.com>
2018-01-08 18:22:30 +0800

    修改代理创建正则

--- rotta-agent/src/components/createbtn.vue
+++ rotta-agent/src/components/createbtn.vue
@@ -74,7 +74,7 @@ export default {
       }
     }, // 配置代理信息
     gosuccom () {
-      if (this.$store.state.checkform.points === false || this.$store.state.checkform.rate === false || this.$store.state.checkform.vedioMix === false || this.$store.state.checkform.liveMix === false) {
+      if (this.$store.state.checkform.points === false || this.$store.state.checkform.rate === false) {
         this.$message({
           message: '请完善配置信息',
           type: 'error'
