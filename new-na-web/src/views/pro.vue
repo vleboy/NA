d3718a8c545d7a8ff71a17e5774e90f1834de853
MistySky <2251154218@qq.com>
2018-01-12 20:19:34 +0800

    更新官网

--- new-na-web/src/views/pro.vue
+++ new-na-web/src/views/pro.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="pro">
     <div class="pro-desc">
-      <img src="/static/proTopBg.jpg" alt="">
+      <router-link to="/"><img src="/static/proTopBg.jpg" alt=""></router-link>
       <div class="pro-desc-msg">
         <h3>NA Gaming</h3>
         <h2>旗舰版介绍</h2>
@@ -11,8 +11,10 @@
     </div>
 
     <div class="pro-cooperation">
-      <h2>NA Gaming旗舰版 战略合作机构</h2>
-      <p>NA Gaming 旗舰版与博彩业官方管理机构、产品验证机构、各大游戏平台达成战略合作，共建手机移动端的线上赌场。</p>
+      <div class="pro-title">
+        <h2>NA Gaming旗舰版 战略合作机构</h2>
+        <p>NA Gaming 旗舰版与博彩业官方管理机构、产品验证机构、各大游戏平台达成战略合作，共建手机移动端的线上赌场。</p>
+      </div>
       <div class="pro-cooperation-companys">
         <ul>
           <li>产品验证机构:</li>
@@ -26,7 +28,7 @@
           <li><img src="/static/ncgac.jpg" alt=""></li>
         </ul>
         <ul>
-          <li>各大游戏平台：</li>
+          <li>各大游戏平台: </li>
           <li><img src="/static/pt.png" alt=""></li>
           <li><img src="/static/sg.jpg" alt=""></li>
           <li><img src="/static/shalong.jpg" alt=""></li>
@@ -34,7 +36,7 @@
           <li><img src="/static/ttg.jpg" alt=""></li>
         </ul>
         <ul>
-          <li></li>
+          <li style="visibility: hidden">各大游戏平台: </li>
           <li><img src="/static/hb.jpg" alt=""></li>
           <li><img src="/static/ipm.jpg" alt=""></li>
           <li><img src="/static/mg.png" alt=""></li>
@@ -45,8 +47,10 @@
     </div>
 
     <div class="pro-login">
-      <h2>NA Gaming旗舰版 战略合作机构</h2>
-      <p>NA Gaming 旗舰版与博彩业官方管理机构、产品验证机构、各大游戏平台达成战略合作，共建手机移动端的线上赌场。</p>
+      <div class="pro-title">
+        <h2>NA Gaming旗舰版 战略合作机构</h2>
+        <p>NA Gaming 旗舰版与博彩业官方管理机构、产品验证机构、各大游戏平台达成战略合作，共建手机移动端的线上赌场。</p>
+      </div>
       <img src="/static/login.jpg" alt="">
       <div class="login-info">
         <div class="login-info-desc">填写用户信息及邀请码即可加入游戏平台。邀请码是一串商户专用的代码，可由商户发放给用户，该代码主要是用来匹配用户到商户线路游戏。</div>
@@ -55,8 +59,10 @@
     </div>
 
     <div class="pro-hall">
-      <h2>NA Gaming 旗舰版 游戏大厅列表</h2>
-      <p>登录后即可进入NA Gaming游戏大厅，游戏大厅将NA与各平台的游戏分类，分别是：真人LIVE、<br>电子游戏、街机游戏、棋牌游戏与体育竞技。玩家可选择自己喜好的类型进入。</p>
+      <div class="pro-title">
+        <h2>NA Gaming 旗舰版 游戏大厅列表</h2>
+        <p>登录后即可进入NA Gaming游戏大厅，游戏大厅将NA与各平台的游戏分类，分别是：真人LIVE、<br>电子游戏、街机游戏、棋牌游戏与体育竞技。玩家可选择自己喜好的类型进入。</p>
+      </div>
       <div class="pro-hall-item">
         <img src="/static/hall1.jpg" alt="">
         <div class="pro-hall-msg">点击任意游戏类别进入，<br>大厅将列出该游戏类别的平台公司，<br>玩家可选择喜爱的公司游戏。</div>
@@ -69,22 +75,28 @@
     </div>
 
     <div class="pro-recharge">
-      <h2>NA Gaming 旗舰版 玩家充值</h2>
-      <p>NA Gaming 在大厅中链接商户充值端口，不在需要打开商户网站等繁琐操作来充值，只需点击筹码旁的+号，<br>即可跳转到商户的充值页面进行在线充值。</p>
+      <div class="pro-title">
+        <h2>NA Gaming 旗舰版 玩家充值</h2>
+        <p>NA Gaming 在大厅中链接商户充值端口，不在需要打开商户网站等繁琐操作来充值，只需点击筹码旁的+号，<br>即可跳转到商户的充值页面进行在线充值。</p>
+      </div>
       <img src="/static/recharge.png" alt="">
       <div class="pro-recharge-msg">为了玩家的充值安全，跳转后的页面，<br>需要玩家再次输入账号密码等信息登陆一次，即可完成充值。</div>
     </div>
 
     <div class="pro-placard">
-      <h2>NA Gaming 旗舰版 系统功能 — 公告</h2>
-      <p>NA Gaming 登陆后，系统会默认打开公告板，强制用户查看，关闭后，也可手动点击大厅下方菜单《活动》按钮打开，<br>公告板分为系统公告和商户公告两个板块，一般用于大型活动或商户充值优惠、限免等内容的发布。</p>
+      <div class="pro-title">
+        <h2>NA Gaming 旗舰版 系统功能 — 公告</h2>
+        <p>NA Gaming 登陆后，系统会默认打开公告板，强制用户查看，关闭后，也可手动点击大厅下方菜单《活动》按钮打开，<br>公告板分为系统公告和商户公告两个板块，一般用于大型活动或商户充值优惠、限免等内容的发布。</p>
+      </div>
       <div class="pro-warning">*旗舰版中，将给予商户更换公告的权限，商户可自由更换商户公告板块的内容。</div>
       <img src="/static/waring.jpg" alt="">
     </div>
 
     <div class="pro-shop">
-      <h2>NA Gaming 旗舰版 系统功能 — 商店系统</h2>
-      <p>NA Gaming 会不定期更新商店内容，包括游戏游戏房卡、道具、礼品等。旗舰版将开放商户调价权限，<br>商户可根据用户的喜好与购买量数据，调整商品价格，以创造更多利润</p>
+      <div class="pro-title">
+        <h2>NA Gaming 旗舰版 系统功能 — 商店系统</h2>
+        <p>NA Gaming 会不定期更新商店内容，包括游戏游戏房卡、道具、礼品等。旗舰版将开放商户调价权限，<br>商户可根据用户的喜好与购买量数据，调整商品价格，以创造更多利润</p>
+      </div>
       <div class="pro-shop-main">
         <div class="pro-shop-cover">
           <img src="/static/shop.jpg" alt="">
@@ -95,14 +107,18 @@
 
     <div class="pro-bottom">
       <div class="pro-bottom-item">
-        <h2>NA Gaming 旗舰版 后台管理 — 多维度报</h2>
-        <p>NA Gaming 在后台管理系统中设定了多维度的报表记录，并依托强大的全球服务器，报表在不删除的情况下，<br>可记录超过200年时间，满足商户随时查阅并计算账目等工作。</p>
+        <div class="pro-title">
+          <h2>NA Gaming 旗舰版 后台管理 — 多维度报</h2>
+          <p>NA Gaming 在后台管理系统中设定了多维度的报表记录，并依托强大的全球服务器，报表在不删除的情况下，<br>可记录超过200年时间，满足商户随时查阅并计算账目等工作。</p>
+        </div>
         <img src="/static/bottom1.png" alt="">
       </div>
 
       <div class="pro-bottom-item">
-        <h2>NA Gaming 旗舰版 后台管理 — 玩家管理</h2>
-        <p>玩家管理页面详细记录了玩家的信息与状态，并可在此页面对管理进行管理操作，也可点击玩家进入详情，查看玩家在各个游戏中的流水与状态。</p>
+        <div class="pro-title">
+          <h2>NA Gaming 旗舰版 后台管理 — 玩家管理</h2>
+          <p>玩家管理页面详细记录了玩家的信息与状态，并可在此页面对管理进行管理操作，也可点击玩家进入详情，查看玩家在各个游戏中的流水与状态。</p>
+        </div>
         <img src="/static/bottom2.png" alt="">
       </div>
     </div>
@@ -157,11 +173,9 @@ export default {
       display flex
       margin 30px 0
       li:first-child
-        // width 10%
         font-size 24px
         margin 0
       li
-        width 15%
         margin 0 25px
         img
           max-width 100%
@@ -303,8 +317,136 @@ export default {
     max-width 100%
 
 @media (max-width: 768px)
+  .pro
+    overflow hidden
+
   .pro-desc-msg
     width 100%
+    h3
+      margin 20px 0 10px
+      font-size 16px
+    h2
+      font-size 21px
+      margin-bottom 25px
+    p
+      font-size 12px
+      padding 0 10%
+  
+  .pro-cooperation
+    padding 0 
+    .pro-cooperation-companys
+      ul
+        li:first-child
+          font-size 12px
+          margin 0
+          text-align center
+        li
+          width 15%
+          margin 0 1%
+
+  .pro-title
+    background #363A46
+    padding 30px 4%
+    h2, p
+      color #ffffff
+    h2
+      font-size 18px
+      margin-bottom 10px
+    p
+      font-size 12px
+
+  .pro-login
+    margin-top 50px
+    padding 0
+    img
+      padding 0 4%
+      
+  
+  .login-info
+    display flex
+    flex-direction column-reverse
+    margin-top 0
+    img
+      width 100%
+      margin-top 20px
+
+  .login-info-desc
+    width 100%
+    font-size 18px
+    margin-top 30px
+
+  .pro-hall
+    padding 0
+    margin-top 50px
+    p
+      margin-bottom 0
+    img 
+      margin 0
+      padding 0 4%
+
+
+  .pro-hall-item
+    margin-top 40px
+    flex-direction: column
+
+  .pro-hall-msg
+    margin 30px 4%
+    width 100%
+    padding 0
+    font-size 18px
+
+  .pro-recharge
+    padding 0
+    img
+      margin-top 40px
+      padding 0 4%
+    p
+      margin-bottom 0
+
+  .pro-recharge-msg
+    margin 30px 4%
+    width 100%
+    padding 0
+    font-size 18px
+  
+  .pro-placard
+    padding 0
+    img
+      padding 0 4%  
+
+  .pro-warning
+    font-size 13px
+    padding 0 4%
+    margin 20px 0 50px 0
+
+  .pro-shop
+    padding 0
+    p
+      margin-bottom 0
+    img
+      margin-top 40px
+      padding 0 4%
+
+  .pro-shop-main
+    flex-direction column
+
+  .pro-shop-msg
+    padding 0 4%
+    font-size 18px
+
+  .pro-bottom
+    margin-top 100px
+    padding 0
+    background #ffffff
+    p
+      margin-bottom 0
+    img
+      margin-top 40px
+      padding 0 4%
+  .pro-bottom-item
+    padding 0
+    margin-top 50px
+    
 </style>
 
 
