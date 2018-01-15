756936b893917b3a84a960de4e042641f32c9b7f
fu wei <592197848@qq.com>
2018-01-15 22:58:01 +0800

    暂时隐藏几个报表

--- rotta-admin/src/components/sidebar-admin.vue
+++ rotta-admin/src/components/sidebar-admin.vue
@@ -8,12 +8,12 @@
               <el-menu-item index="personal" v-show="userRight.personal.hasRight">个人中心</el-menu-item>
               <el-submenu index="1">
                   <template slot="title">输赢报表</template>
-                  <el-menu-item index="allReport">公司输赢总报表</el-menu-item>
+                  <!-- <el-menu-item index="allReport">公司输赢总报表</el-menu-item> -->
                   <el-submenu index="1-1">
                       <template slot="title">NA游戏报表</template>
-                      <el-menu-item index="naAllGameReport">NA游戏总报表</el-menu-item>
+                      <!-- <el-menu-item index="naAllGameReport">NA游戏总报表</el-menu-item> -->
                       <el-menu-item index="naVedioGameReport">NA电子游戏报表</el-menu-item>
-                      <el-menu-item index="naLiveGameReport">NA真人游戏报表</el-menu-item>
+                      <!-- <el-menu-item index="naLiveGameReport">NA真人游戏报表</el-menu-item> -->
                       <el-menu-item index="naArcadeGameReport">NA街机游戏报表</el-menu-item>
                       <el-menu-item index="naMallReport">NA商城总报表</el-menu-item>
                   </el-submenu>
