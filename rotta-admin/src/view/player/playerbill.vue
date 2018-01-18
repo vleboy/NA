1c22dfebbc96ffa6f6b1f91108b8dbadf81d2729
gp <gp646890523@sina.com>
2018-01-18 16:00:40 +0800

    update ( rotta ) 处理玩家流水账下注时间没有返回情况

--- rotta-admin/src/view/player/playerbill.vue
+++ rotta-admin/src/view/player/playerbill.vue
@@ -164,7 +164,7 @@ export default {
   },
   methods: {
     formatterTime (row) {
-      return detailTime(row)
+      return row ? detailTime(row) : '-'
     }, // 格式化创建时间
     getNowsize (size) {
       this.nowSize = size
