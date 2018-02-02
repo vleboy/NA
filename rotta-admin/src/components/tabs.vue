b7fb32d215b0dd7512d8b76bc806ab241c6a4499
MistySky <2251154218@qq.com>
2018-02-03 00:19:54 +0800

    update

--- rotta-admin/src/components/tabs.vue
+++ rotta-admin/src/components/tabs.vue
@@ -52,7 +52,7 @@
 			onTabClick(data) {
 				this.$store.commit('resetPartLoading')
 				this.$router.push({ path: this.visitedViews[data.index].path })
-				// console.log(data.name)
+				//console.log(data.name)
 				this.$store.dispatch('onTabClick', data.name)
 			}
 		},
