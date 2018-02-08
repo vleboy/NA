15b3c4e81f25aad3870eb7233642f07a560ba89e
fu wei <592197848@qq.com>
2018-02-08 18:17:21 +0800

    调整告警列表

--- rotta-admin/src/view/agent/point-warning/pointWarning.vue
+++ rotta-admin/src/view/agent/point-warning/pointWarning.vue
@@ -19,11 +19,6 @@
               <span v-else>{{scope.row.displayName}}</span>
             </template>
           </el-table-column>
-          <el-table-column label="抽成比" prop="" align="left">
-            <template scope="scope">
-              <span>{{scope.row.rate}}%</span>
-            </template>
-          </el-table-column>
           <el-table-column label="接入商" prop="" align="center">
             <template scope="scope">
               <div slot="reference" class="gameName">
