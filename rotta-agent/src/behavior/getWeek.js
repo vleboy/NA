4e5e2987e231eafe7942780dbbb2a02bebe1ef06
fu wei <592197848@qq.com>
2018-02-12 10:47:03 +0800

    修改时间判断

--- rotta-agent/src/behavior/getWeek.js
+++ rotta-agent/src/behavior/getWeek.js
@@ -4,8 +4,11 @@ export const getWeek = function () {
 
     let startTime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate() + 1 - nowDay)
     let endTime = new Date().getTime()
-    
+
     startTime = startTime.setHours(11, 59, 59, 0)
     endTime =  endTime - 180000
+
+    startTime - endTime > 0 ? startTime = new Date().setHours(0, 0, 0, 0) : ''
+    
     return [startTime, endTime]
   } // 处理周次
